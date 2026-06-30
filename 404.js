f78366745b7043136126aa005984d3fdad5cb7c0
Krishna chauhan <chauhan2024k@gmail.com>
2026-06-30 23:20:44 +0530

    Add files via upload

---
+++ 404.js
@@ -0,0 +1,28 @@
+import express from "express"
+import path from 'path'
+const app =express();
+const abspath=path.resolve('view');
+
+app.get("/",(req,resp) =>{
+    
+    resp.sendFile(abspath+"/home.html");
+})
+
+app.get("/login",(req,resp) =>{
+
+    resp.sendFile(abspath+"/login.html");
+})
+
+app.get("/about",(req,resp) =>{
+    
+    resp.sendFile(abspath+"/about.html");
+})
+
+//agar hamara page found na ho to 404 me ye use kar sakte hai taki user ko ye lage kii url galt hai
+app.use((req,resp)=>{
+
+resp.sendFile(abspath+"/404.html");
+})
+
+app.listen(3200);
+console.log("okk");
